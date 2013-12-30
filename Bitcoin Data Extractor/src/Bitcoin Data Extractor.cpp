7fc466475dfb7e9ef97424947555d8e180e18ae2
Zehady Abdullah Khan <a_khanzehady@yahoo.com>
2013-12-31 07:26:33 +0900

    Initial Commit

---
+++ Bitcoin Data Extractor/src/Bitcoin Data Extractor.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Bitcoin.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include "CommonHeaders.h"
+#include "RichGetsRicher.h"
+#include "ExtractFromBlockExplorer.h"
+
+
+int main(){
+	//numberOfAddressesOverWeeks();
+	//run extractor for block explorer to create graph
+	runExtractor();
+	//cout << "hello";
+}
+
